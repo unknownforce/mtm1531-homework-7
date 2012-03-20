5ee32cee0c4eeda54299123cf536891c6e6812c8
Petrus Chan <petchan34@gmail.com>
2012-03-20 00:47:40 -0400

    added Tennis Court Data Set to the App

---
+++ includes/db.php
@@ -0,0 +1,12 @@
+<?php
+	
+	//Gets an environment variable we created in the .htaccess file
+	//This is the best way to keep username and passwords out of public GitHub repos
+	$user = getenv('DB_USER'); // echo $user; // Check if it works
+	$pass = getenv('DB_PASS');
+	$dsn = stripslashes(getenv('DB_DSN'));
+	
+	// Opens a connection to the database and stores it in a variable
+	$db = new PDO($dsn, $user, $pass);
+	// Makes sure we talk to the database in UTF-8, so we can support more than just English
+	$db->exec('SET NAMES utf8');
